9e42f9ab39f1af9c0f4136ec798e33e334832dff
Mary Ojo <maryojo3@gmail.com>
2023-04-29 15:03:27 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
       <Spline className="block lg:hidden" scene="https://prod.spline.design/pjV1dsCkZ6BnD2-i/scene.splinecode" />
       <div className="absolute top-6 lg:top-10 left-5 lg:left-10 w-32"><img src={logo}/></div>
       <div className="absolute top-24 right-10 lg:top-10 lg:right-36 w-5/6 md:w-4/6 lg:w-3/6 flex flex-col gap-4">
-        <h1 className="text-zinc-100">Hi!</h1>
+        <h1 className="text-zinc-900">Hi!</h1>
         <div>
         <p>I'm working really hard to build an incredible portfolio and I can't wait to share with you!</p>
         <p>Meanwhile you can check out these links</p>
@@ -23,23 +23,23 @@ export default function App() {
         <div className="flex flex-wrap gap-x-10 gap-y-3">
           <div className="flex flex-row items-center gap-2">
           <SiPolywork className="icon"/>
-            <a href="https://www.polywork.com/maryojo">My recent activities</a>
+            <a href="https://www.polywork.com/maryojo" target="_blank" rel="noreferrer">My recent activities</a>
           </div>
           <div className="flex flex-row items-center gap-2">
           <CiLinkedin className="icon"/>
-            <a href="#">My LinkedIn profile</a>
+            <a href="#" target="_blank" rel="noreferrer">My LinkedIn profile</a>
           </div>
           <div className="flex flex-row items-center gap-2">
           <SlSocialGithub className="icon"/>
-            <a href="#">My coding activities</a>
+            <a href="#" target="_blank" rel="noreferrer">My coding activities</a>
           </div>
           <div className="flex flex-row items-center gap-2">
           <SlSocialBehance className="icon"/>
-            <a href="#">My design </a>
+            <a href="#" target="_blank" rel="noreferrer">My design </a>
           </div>
           <div className="flex flex-row items-center gap-2">
           <TbMailbox className="icon"/>
-          <p>Send me a mail on <a>hi@maryojo.me</a></p>
+          <p>Send me a mail on <a href="#" target="_blank" rel="noreferrer">hi@maryojo.me</a></p>
           </div>
         </div>
       </div>
