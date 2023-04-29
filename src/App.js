d4684239e6d2238b4c99577147549903c5d6d856
Mary Ojo <maryojo3@gmail.com>
2023-04-29 14:40:04 +0100

    Fix responsiveness

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ export default function App() {
     
       <Spline className="hidden lg:block" scene="https://prod.spline.design/ucDueTh0omand6Xu/scene.splinecode" />
       <Spline className="block lg:hidden" scene="https://prod.spline.design/pjV1dsCkZ6BnD2-i/scene.splinecode" />
-      {/* <div className="absolute top-10 left-10"><img src={logo}/></div> */}
-      <div className="absolute top-6 right-10 lg:top-10 lg:right-36 w-5/6 md:w-4/6 lg:w-3/6 flex flex-col gap-4">
-        <h1>Hi there!</h1>
+      <div className="absolute top-6 lg:top-10 left-5 lg:left-10 w-32"><img src={logo}/></div>
+      <div className="absolute top-24 right-10 lg:top-10 lg:right-36 w-5/6 md:w-4/6 lg:w-3/6 flex flex-col gap-4">
+        <h1 className="text-zinc-100">Hi!</h1>
         <div>
-        <p className="text-zinc-600">I'm working really hard to build an incredible portfolio and I can't wait to share with you!</p>
-        <p className="text-zinc-600">Meanwhile you can check out these links</p>
+        <p>I'm working really hard to build an incredible portfolio and I can't wait to share with you!</p>
+        <p>Meanwhile you can check out these links</p>
         </div>
         
         <div className="flex flex-wrap gap-x-10 gap-y-3">
           <div className="flex flex-row items-center gap-2">
           <SiPolywork className="icon"/>
-            <a href="#">My recent activities</a>
+            <a href="https://www.polywork.com/maryojo">My recent activities</a>
           </div>
           <div className="flex flex-row items-center gap-2">
           <CiLinkedin className="icon"/>
